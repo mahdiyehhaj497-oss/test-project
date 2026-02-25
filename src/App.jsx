f880552940a72347476306a4e65f79e11218d3a7
mahdiyeh <mahdiyehhaj497@gmail.com>
2026-02-25 19:33:57 +0330

    photos

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,12 @@ import InputBox from "./component/headers/inputBox";
 export default function App() {
   return (
     <>
-      <div className="bg-gray-800 h-screen ">
+      <div className= "w-full h-screen bg-[linear-gradient(to_right,rgba(0,0,0,0.6),rgba(0,0,0,0)),url(src/assets/photo/istockphoto.jpg)] bg-cover bg-center">
         <div className="container-custom">
           <Header />
           <InputBox />
           <FollowUs />
           <HeaderSlider />
-          
         </div>
       </div>
     </>
--- src/component/headers/headerSlider.jsx
+++ src/component/headers/headerSlider.jsx
@@ -11,27 +11,27 @@ export default function HeaderSlider() {
             spaceBetween={30}
             slidesPerView={4}
             onSlideChange={() => console.log("slide change")}
-                    onSwiper={(swiper) => console.log(swiper)}
-                    autoplay={{delay:2000}}
-                    loop
+            onSwiper={(swiper) => console.log(swiper)}
+            autoplay={{ delay: 2000 }}
+            loop
           >
             <SwiperSlide>
-            
+              <img src="src/assets/photo/film.jpg" alt="" />
             </SwiperSlide>
             <SwiperSlide>
-             
+              <img src="src/assets/photo/film.jpg" alt="" />
             </SwiperSlide>
             <SwiperSlide>
-            
+              <img src="src/assets/photo/film.jpg" alt="" />
             </SwiperSlide>
             <SwiperSlide>
-             
+              <img src="src/assets/photo/film.jpg" alt="" />
             </SwiperSlide>
             <SwiperSlide>
-            
+              <img src="src/assets/photo/film.jpg" alt="" />
             </SwiperSlide>
             <SwiperSlide>
-             
+              <img className="border-1 border-slate-700" src="src/assets/photo/film.jpg" alt="" />
             </SwiperSlide>
             ...
           </Swiper>
